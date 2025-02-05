d2730d1c01d4e7ed03cf46f1e2135d64dcc1b43c
Sweet Bitter <1826297652@qq.com>
2025-02-05 17:48:53 +0800

    完成磁盘分区信息的创建与发送

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -5,6 +5,7 @@
 #include "framework.h"
 #include "RemoteCtrl.h"
 #include "ServerSocket.h"
+#include <direct.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -16,6 +17,35 @@
 CWinApp theApp;
 
 using namespace std;
+void Dump(BYTE* pData, size_t nSize) {
+    std::string strOut;
+    for (size_t i = 0; i < nSize; i++) {
+        char buf[8] = "";
+        if (i > 0 && (i % 16 == 0)) strOut += "\n";
+        snprintf(buf, sizeof(buf), "%02X ", pData[i] & 0xFF);
+        strOut += buf;
+    }
+    strOut += "\n";
+    //std::cout << strOut.c_str() << std::endl;
+    OutputDebugStringA(strOut.c_str());
+}
+
+int MakeDriverInfo() {      //driver从1开始，1是a盘（软盘），2是b盘（软盘），3是c盘...26是z盘
+    std::string result;
+    for (int i = 1; i <= 26; i++) {
+        if (_chdrive(i) == 0) {
+            if (result.size() > 0) {
+                result += ',';
+            }
+            result += 'A' + i - 1;
+        }
+    }
+
+    CPacket pack(1, (BYTE*)result.c_str(), result.size());      //打包用的
+    Dump((BYTE*)pack.Data(), pack.Size());
+    //CServerSocket::getInstence()->Send(pack);
+    return 0;
+}
 
 int main()
 {
@@ -36,23 +66,32 @@ int main()
         {
             // TODO: socket,bind,listen,accept,read,write,close
             //init sock
-            CServerSocket* pserver = CServerSocket::getInstence();
-            if (pserver->InitSocket() == false) {
-                MessageBox(NULL, _T("网络初始化，请检查网络状态"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);
-                exit(0);
-            }
-            int count = 0;
-            while (CServerSocket::getInstence() != NULL) {
-                if (pserver->AcceptClient() == false) {
-                    if (count >= 3) {
-                        MessageBox(NULL, _T("重试超时"), _T("请稍后再试！"), MB_OK | MB_ICONERROR);
-                        exit(0);
-                    }
-                    MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-                    count++;
-                }
-                int ret = pserver->DealCommand();
-                //TODO:
+            //CServerSocket* pserver = CServerSocket::getInstence();
+            //if (pserver->InitSocket() == false) {
+            //    MessageBox(NULL, _T("网络初始化，请检查网络状态"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);
+            //    exit(0);
+            //}
+            //int count = 0;
+            //while (CServerSocket::getInstence() != NULL) {
+            //    if (pserver->AcceptClient() == false) {
+            //        if (count >= 3) {
+            //            MessageBox(NULL, _T("重试超时"), _T("请稍后再试！"), MB_OK | MB_ICONERROR);
+            //            exit(0);
+            //        }
+            //        MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+            //        count++;
+            //    }
+            //    int ret = pserver->DealCommand();
+            //    //TODO:
+            //}
+            int nCmd = 1;
+            switch (nCmd)
+            {
+            case 1:     //查看磁盘分区
+                MakeDriverInfo();
+                break;
+            default:
+                break;
             }
             
         }
--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -2,9 +2,22 @@
 #include "pch.h"
 #include "framework.h"
 
+#pragma pack(push)
+#pragma pack(1)
 class CPacket {
 public:
 	CPacket() : sHead(0), nLength(0), sCmd(0), sSum(0) {}
+	CPacket(WORD nCmd, const BYTE* pData, size_t nSize) {
+		sHead = 0xFEFF;
+		nLength = nSize + 4;
+		sCmd = nCmd;
+		strData.resize(nSize);
+		memcpy((void*)strData.c_str(), pData, nSize);
+		sSum = 0;
+		for (size_t j = 0; j < strData.size(); j++) {
+			sSum += BYTE(strData[j]) & 0xFF;
+		}
+	}
 	CPacket(const CPacket& pack) {
 		sHead = pack.sHead;
 		nLength = pack.nLength;
@@ -58,15 +71,30 @@ public:
 		}
 		return *this;
 	}
+
+	int Size() {	//包数据大小
+		return nLength + 6;
+	}
+	const char* Data() {
+		strOut.resize(nLength + 6);
+		BYTE* pData = (BYTE*)strOut.c_str();
+		*(WORD*)(pData) = sHead; pData += 2;
+		*(WORD*)(pData) = nLength; pData += 4;
+		*(WORD*)(pData) = sCmd; pData += 2;
+		memcpy(pData, strData.c_str(), strData.size()); pData += strData.size();
+		*(WORD*)pData = sSum;
+		return strOut.c_str();
+	}
 public:
 	WORD sHead;				//固定位 FE FF
 	DWORD nLength;			//包长度
 	WORD sCmd;				//控制命令
 	std::string strData;	//包数据
 	WORD sSum;				//和校验
+	std::string strOut;		//整个包的数据
 
 };
-
+#pragma pack(pop)
 class CServerSocket
 {
 public:
@@ -127,6 +155,10 @@ public:
 	bool Send(const char* pData, size_t nSize) {
 		if (m_clnt == -1) return false;
 		return send(m_clnt, pData, nSize, 0) > 0;
+	}
+	bool Send(CPacket& pack) {
+		if (m_clnt == -1) return false;
+		return send(m_clnt, pack.Data(), pack.Size(), 0) > 0;
 	}
 private:
 	SOCKET m_clnt, m_sock;
